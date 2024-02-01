82710dfeb8219410c537f7b606ed4c251adff690
orosmatthew <orosmatthew@pm.me>
2024-02-01 12:16:10 -0500

    Fix warnings

--- app/src/main/java/site/pixeled/vibevault/WelcomeFragment.kt
+++ app/src/main/java/site/pixeled/vibevault/WelcomeFragment.kt
@@ -1,14 +1,13 @@
 package site.pixeled.vibevault
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 
 class WelcomeFragment : Fragment() {
