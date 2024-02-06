d3bbe9d345bdbc5aa596d995b40a70a5b4148323
orosmatthew <orosmatthew@pm.me>
2024-02-06 12:46:09 -0500

    Clean up warnings

--- app/src/main/java/site/pixeled/vibevault/LogDetailFragment.kt
+++ app/src/main/java/site/pixeled/vibevault/LogDetailFragment.kt
@@ -1,13 +1,12 @@
 package site.pixeled.vibevault
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import site.pixeled.vibevault.models.MoodData
 import site.pixeled.vibevault.types.moodTypeStringId
--- app/src/main/java/site/pixeled/vibevault/LogFragment.kt
+++ app/src/main/java/site/pixeled/vibevault/LogFragment.kt
@@ -19,10 +19,6 @@ class LogFragment : Fragment() {
 
     private var mSelectedMood: MoodType? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
